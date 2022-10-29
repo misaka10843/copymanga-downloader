4147dbe0b9fcd0b31a9c0a1f508e6aaf538db8cf
misaka10843 <misaka10843@outlook.jp>
2022-10-29 23:04:45 +0800

    更新input与print

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ from retrying import retry
 import requests
 from tqdm import tqdm
 
+from rich import print as print
+from rich.prompt import Prompt
+
 import datetime
 
 now = datetime.datetime.now()
@@ -29,34 +32,33 @@ api_headers = {
 }
 proxies = {}
 
-
 def get_url():
     # *从GitHub(首先两个为CDN，方便中国大陆用户获取)中获取url的信息
     response = requests.get(
         'https://raw.fastgit.org/misaka10843/copymanga-downloader/master/url.json',
         headers=headers)
     if response.status_code != 200:
-        print("您的网络似乎不支持fastgit，我们正在切换新的请求地址")
+        print("您的网络似乎[italic red]不支持fastgit[/italic red]，我们正在切换新的请求地址")
         response = requests.get(
             'https://fastly.jsdelivr.net/gh/misaka10843/copymanga-downloader@master/url.json',
             headers=headers)
         if response.status_code != 200:
-            print("您的网络似乎也不支持jsdelivr，我们只能请求GitHub原站了")
+            print("您的网络似乎也[italic red]不支持jsdelivr[/italic red]，我们只能请求GitHub原站了")
             response = requests.get(
                 'https://raw.githubusercontent.com/misaka10843/copymanga-downloader/master/url.json',
                 headers=headers)
             if response.status_code != 200:
-                print("我们无法获取到相关信息，请检查网络后重试")
+                print("[italic red]我们无法获取到相关信息，请检查网络后重试[/italic red]")
                 # *直接退出
                 exit()
     url_list = response.json()
-    print("我们获取到copymanga有以下域名，请选择您希望使用的域名来确保连接通畅:")
+    print("[italic yellow]我们获取到copymanga有以下域名，请选择您希望使用的域名来确保连接通畅:[/italic yellow]")
     list_num = 0
     for i in url_list:
         # *循环输出url信息
         print(list_num, '->', i)
         list_num = list_num + 1
-    get_url_list_num = input("您希望通过第几个域名来获取相关信息?(默认0)：")
+    get_url_list_num = Prompt.ask("[italic yellow]您希望通过第几个域名来获取相关信息?[/italic yellow][italic green](默认0)[/italic green]")
     if len(get_url_list_num) == 0:
         get_url_list_num = 0
     # *从用户选择中获取到url信息并返回
@@ -79,27 +81,27 @@ def get_settings():
     # *如果为第一次初始化
     if first_initialization == 1:
         json_data = {}
-        download_path = input(
-            "您似乎是第一次启动此程序，请您先输入您需要下载的路径(请输入E:\\manga这种格式,不要最后一个斜杠哦qwq)：")
+        download_path = Prompt.ask(
+            "您似乎是第一次启动此程序，请您先输入您需要下载的路径[italic yellow](请输入E:\\manga这种格式,不要最后一个斜杠哦qwq)[/italic yellow]")
         # *将反斜杠转成正斜杠
         json_data["download_path"] = download_path.replace('\\', '/')
-        print("\n接下来填写的是获取您的收藏漫画需要的参数，请认真填写哦qwq(如果不想获取的话也可以直接填写null)\n")
-        cookies_get = input(
-            "请输入您的authorization(如不会获取请看https://t.hk.uy/bdFu ("
-            "此为获取用户收藏漫画,下载服务不受影响))：")
+        print("\n接下来填写的是获取您的收藏漫画需要的参数，请认真填写哦qwq[italic yellow](如果不想获取的话也可以直接填写null)[/italic yellow]\n")
+        cookies_get = Prompt.ask(
+            "请输入您的authorization[italic yellow](如不会获取请看https://t.hk.uy/bdFu ("
+            "此为获取用户收藏漫画,下载服务不受影响))[/italic yellow]")
         json_data["authorization"] = cookies_get
-        if input("是否使用海外CDN？(y/n)：").lower() == 'y':
+        if Prompt.ask("是否使用海外CDN？[italic green](y/n)[/italic green]").lower() == 'y':
             json_data["use_oversea_cdn"] = True
         else:
             json_data["use_oversea_cdn"] = False
         # *是否为了节省服务器带宽而使用webp
-        if input("是否下载webp格式图片(可以节省服务器资源)？(y/n)：").lower() == 'y':
+        if Prompt.ask("是否下载webp格式图片[italic yellow](可以节省服务器资源)？[/italic yellow][italic green](y/n)[/italic green]").lower() == 'y':
             json_data["use_webp"] = True
         else:
             json_data["use_webp"] = False
         # *获取proxies状态
-        proxies_get = input(
-            "您是否使用了代理？如果是，请填写代理地址(如http://127.0.0.1:8099或者socks5://127.0.0.1:8099)：")
+        proxies_get = Prompt.ask(
+            "您是否使用了代理？如果是，请填写代理地址[italic yellow](如http://127.0.0.1:8099或者socks5://127.0.0.1:8099，没有请直接回车)[/italic yellow]")
         json_data["proxies"] = proxies_get
         # *获取最新的域名状态
         json_data["api_url"] = get_url()
@@ -107,13 +109,13 @@ def get_settings():
         with open('./settings.json', 'w', encoding="utf-8") as fp:
             json.dump(json_data, fp, indent=2, ensure_ascii=False)
 
-        print("恭喜您已经完成初始化啦！\n我们将立即执行主要程序，\n如果您需要修改设置的话可以直接到程序根目录的settings.json更改qwq")
+        print("[italic green]恭喜您已经完成初始化啦！[/italic green]\n我们将立即执行主要程序，\n[italic blue]如果您需要修改设置的话可以直接到程序根目录的settings.json更改qwq[/italic blue]")
     # *读取设置内容
     with open('./settings.json', 'r', encoding="utf-8") as fp:
         json_data = json.load(fp)
         #! 只要下载路径/请求地址是空，那么就直接报错
         if not json_data["download_path"] or not json_data["api_url"]:
-            print("\033[1;31m 您的设置似乎出现了问题导致部分设置丢失，请您重新启动此程序后重新设置\033[37m")
+            print("[italic red]您的设置似乎出现了问题导致部分设置丢失，请您重新启动此程序后重新设置[/italic red]")
             fp.close()
             os.rename("./settings.json", "./settings_old.json")
             exit()
@@ -145,13 +147,13 @@ def get_settings():
 
 def manga_search(manga_name):
     global get_list_name, get_list_manga
-    print("正在搜索中...\r", end="")
+    print("[italic blue]正在搜索中...[/italic blue]\r", end="")
     # *获取搜索结果
     response = requests.get(
         'https://api.%s/api/v3/search/comic?format=json&limit=20&offset=0&platform=3&q=%s' % (
             Api_url, manga_name),
         headers=api_headers, proxies=proxies)
-    print("搜索完毕啦！  \n")
+    print("[italic green]搜索完毕啦！  [/italic green]\n")
     # !简要判断是否服务器无法连接
     if response.status_code == 200:
         # *将api解析成json
@@ -159,12 +161,12 @@ def manga_search(manga_name):
         # *初始化列表的序号
         list_num = 0
 
-        print("已搜索出以下漫画(如果没有您要找的漫画，请更换关键词即可)：")
+        print("已搜索出以下漫画[italic yellow](如果没有您要找的漫画，请更换关键词即可)[/italic yellow]：")
         # *循环输出搜索列表
         for i in manga_search_list["results"]["list"]:
             print(list_num, '->', i["name"])
             list_num = list_num + 1
-        get_list_num = input("您需要下载的漫画是序号几？(默认0)：")
+        get_list_num = Prompt.ask("您需要下载的漫画是序号几？[italic green](默认0)[/italic green]：")
         if len(get_list_num) == 0:
             get_list_num = 0
         get_list_name = manga_search_list["results"]["list"][int(
@@ -173,9 +175,9 @@ def manga_search(manga_name):
             get_list_num)]["name"]
     else:
         # *报告远程服务器无法连接的状态码
-        print("服务器似乎\033[1;31m 无法连接\033[37m 了qwq\n")
-        print("如需使用代理，可使用命令，如：set https_proxy=http://127.0.0.1:7890，或者在settings.json中设置代理")
-        print("返回的状态码是：%d" % response.status_code)
+        print("[italic yellow]服务器似乎[/italic yellow][italic red]无法连接[/italic red][italic yellow]了qwq[/italic yellow]\n")
+        print("[italic yellow]如需使用代理，可使用命令，如：set https_proxy=http://127.0.0.1:7890，或者在settings.json中设置代理[/italic yellow]")
+        print("[italic yellow]返回的状态码是：%d[/italic yellow]" % response.status_code)
         sys.exit(0)
 
 
@@ -190,7 +192,7 @@ def manga_chapter_group(Manga_pathWord):
         if len(group_list) == 1:
             return "default"
         else:
-            print("我们获取到了一些不同的分组，请输入需要下载的分组序号(默认为‘默認’):")
+            print("我们获取到了一些不同的分组，请输入需要下载的分组序号[italic green](默认为‘默認’)[/italic green]:")
             list_num = 0
             # *循环输出
             while list_num < len(group_list):
@@ -198,7 +200,7 @@ def manga_chapter_group(Manga_pathWord):
                       chapter_group_list["results"]["groups"][group_list[list_num]]["name"])
                 list_num = list_num + 1
             # *获取选项
-            Get_group = input()
+            Get_group = Prompt.ask()
             # *添加默认选项
             if len(Get_group) == 0:
                 Get_group = 0
@@ -219,29 +221,28 @@ def manga_chapter_list():
     if manga_chapter.status_code == 200:
         # *将api解析成json
         chapter_list = manga_chapter.json()
-        print("我们获取了\033[1;33m %s\033[37m 话的内容，请问是如何下载呢？" %
+        print("我们获取了[italic yellow]%s[/italic yellow]话的内容，请问是如何下载呢？" %
               chapter_list["results"]["total"])
         # *判断用户需要怎么下载
-        how_download = input("1->全本下载\n2->范围下载\n3->单话下载\n您的选择是(默认全本下载)：")
+        how_download = Prompt.ask("[bold yellow]1->全本下载\n2->范围下载\n3->单话下载[bold yellow]\n您的选择是[italic green](默认全本下载)[/italic greeb]：")
         all_chapter = 0  # !防止误触发
         if len(how_download) == 0:
             how_download = 1
         if int(how_download) == 1:
             all_chapter = 1
         elif int(how_download) == 2:
-            start_chapter = input("从第几话？")
-            end_chapter = input("到第几话？")
+            start_chapter = Prompt.ask("[bold yellow]从第几话？[/bold yellow]")
+            end_chapter = Prompt.ask("[bold yellow]到第几话？[/bold yellow]")
         elif int(how_download) == 3:
-            start_chapter = end_chapter = input("下载第几话？")
+            start_chapter = end_chapter = Prompt.ask("[bold yellow]下载第几话？[/bold yellow]")
     else:
         # *报告远程服务器无法连接的状态码
-        print("服务器似乎\033[1;31m 无法连接\033[37m 了qwq\n")
-        print("返回的状态码是：%d" % manga_chapter.status_code)
+        print("[italic yellow]服务器似乎[/italic yellow][italic red]无法连接[/italic red][italic yellow]了qwq[/italic yellow]\n")
+        print("[italic yellow]返回的状态码是：%d[/italic yellow]" % manga_chapter.status_code)
         sys.exit(0)
 
 @retry(wait_fixed=3000,stop_max_attempt_number=10)
 def download(url: str, fname: str, img_num: str):
-    
     # 用流stream的方式获取url的数据
     resp = requests.get(url, stream=True, verify=False)
     # 拿到文件的长度，并把total初始化为0
@@ -313,7 +314,7 @@ def manga_download():
         else:
             os.system("clear")
         print(
-            "这个漫画已经全部下载完了qwq                                                     ")
+            "[bold green]这个漫画已经全部下载完了qwq[bold green]")
         # *返回到初始界面
         welcome()
 
@@ -340,15 +341,15 @@ def manga_collection(offset):
     global get_list_name, get_list_manga
     if Authorization == 0:
         print(
-            "您并未填写Authorization，请按照https://t.hk.uy/bdFu填写并添加到settings.json中的authorization字段")
+            "[italic yellow]您并未填写Authorization，请按照https://t.hk.uy/bdFu 填写并添加到settings.json中的authorization字段[/italic yellow]")
         return
     manga_search_list = ""
-    print("正在查询中...\r", end="")
+    print("[italic blue]正在查询中...[italic blue]\r", end="")
     response = requests.get(
         'https://%s/api/v3/member/collect/comics?limit=12&offset={'
         '%s}&free_type=1&ordering=-datetime_modifier' % (Api_url, offset),
         headers=headers, proxies=proxies)
-    print("查询完毕啦！  \n")
+    print("[italic green]查询完毕啦！  [/italic green]\n")
     # !简要判断是否服务器无法连接
     if response.status_code == 200:
         # *将api解析成json
@@ -357,12 +358,12 @@ def manga_collection(offset):
         list_num = 0
 
         # print("已查询出以下漫画(输入pn为下一页，pu为上一页)：")
-        print("已查询出以下漫画(暂且只能查询前50个)：")
+        print("已查询出以下漫画[italic yellow](暂且只能查询前50个)[/italic yellow]：")
         # *循环输出搜索列表
         for i in manga_search_list["results"]["list"]:
             print(list_num, '->', i["comic"]["name"])
             list_num = list_num + 1
-        get_list_num = input("您需要下载的漫画是序号几？(默认0)：")
+        get_list_num = Prompt.ask("您需要下载的漫画是序号几？[italic green](默认0)[/italic green]：")
         if len(get_list_num) == 0:
             get_list_num = 0
         # Todo：查询漫画时翻页功能
@@ -385,8 +386,8 @@ def manga_collection(offset):
         manga_search(get_list_name)
     else:
         # *报告远程服务器无法连接的状态码
-        print("服务器似乎\033[1;31m 无法连接\033[37m 了qwq\n")
-        print("返回的状态码是：%d" % response.status_code)
+        print("[italic yellow]服务器似乎[/italic yellow][italic red]无法连接[/italic red][italic yellow]了qwq[/italic yellow]\n")
+        print("[italic yellow]返回的状态码是：%d[/italic yellow]" % response.status_code)
         sys.exit(0)
 
 
@@ -394,14 +395,14 @@ def manga_collection_backup():
     global get_list_name, get_list_manga
     if Authorization == 0:
         print(
-            "您并未填写Authorization，请按照https://t.hk.uy/bdFu填写并添加到settings.json中的authorization字段")
+            "[italic yellow]您并未填写Authorization，请按照https://t.hk.uy/bdFu 填写并添加到settings.json中的authorization字段[/italic yellow]")
         return
     manga_search_list = ""
-    print("正在查询中...\r", end="")
+    print("[italic blue]正在查询中...[italic blue]\r", end="")
     response = requests.get(
         'https://%s/api/v3/member/collect/comics?limit=500&offset=0&free_type=1&ordering=-datetime_modifier' % Api_url,
         headers=headers, proxies=proxies)
-    print("查询完毕啦！  \n")
+    print("[italic green]查询完毕啦！  [/italic green]\n")
     # !简要判断是否服务器无法连接
     if response.status_code == 200:
         # *将api解析成json
@@ -422,23 +423,23 @@ def manga_collection_backup():
         for line in manga_list:
             f.write('%s\n' % line["comic"]["name"])
         f.close()
-        print("写入完成！(SCV请用Excel打开，编码为GBK)")
+        print("[italic green]写入完成！[/italic green][italic yellow](SCV请用Excel打开，编码为GBK)[/italic yellow]")
         welcome()
     else:
         # *报告远程服务器无法连接的状态码
-        print("服务器似乎\033[1;31m 无法连接\033[37m 了qwq\n")
-        print("返回的状态码是：%d" % response.status_code)
+        print("[italic yellow]服务器似乎[/italic yellow][italic red]无法连接[/italic red][italic yellow]了qwq[/italic yellow]\n")
+        print("[italic yellow]返回的状态码是：%d[/italic yellow]" % response.status_code)
         sys.exit(0)
 
 
 def welcome():
-    is_search = input("您是想搜索还是查看您的收藏？(0:导出收藏,1:搜索,2:收藏  默认1):")
+    is_search = Prompt.ask("您是想搜索还是查看您的收藏？[italic yellow](0:导出收藏,1:搜索,2:收藏  默认1)[/italic yellow]", choices=["0", "1", "2"], default="1")
     if is_search == "2":
         manga_collection(0)
     elif is_search == "0":
         manga_collection_backup()
     else:
-        manga_name = input("请输入漫画名称:")
+        manga_name = Prompt.ask("请输入漫画名称")
         manga_search(manga_name)
 
 
